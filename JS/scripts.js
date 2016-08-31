b6c2928e9ce165994f1f27958129615e608f3bda
Guilherme Henrique Lourenço <ghl.guilherme@gmail.com>
2016-08-31 11:19:45 -0300

    Criação das telas de níveis

---
+++ JS/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#btn-facil").click(function(event){
+        location.href = "facil.html";
+    });
+    $("#btn-medio").click(function(event){
+        location.href = "medio.html";
+    });
+    $("#btn-dificil").click(function(event){
+        location.href = "dificil.html";
+    });
+});
